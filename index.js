509e047c3322cd850464f1caf053d2aae53dbe4f
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-04-07 17:04:04 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+
+
+const names = ['Lisa', 'Kaitlin', 'Jan']
+
+const what = 'surprise'
+
+function writeCards(names, what) {
+    const newArr = [];
+ 
+  for (let i =0; i < names.length; i++) {  
+       newArr.push(`Thank you, ${names[i]}, for the wonderful ${what} gift!`);
+       debugger;
+   }
+    return newArr;
+    
+}
+writeCards(names, what);
+
+
+function countDown(i) {
+while (i>=0) {
+    console.log (i);
+    i--;
+}
+}
+countDown(i);
